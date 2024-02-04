d4d7805b5589eeafed9aa54950aa06b950ab5fc5
Mariusz Bigos <mbrolety@gmail.com>
2024-02-04 20:18:23 +0100

    Add Form, Tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Form from "./Form";
+import Tasks from "./Tasks";
+
+const tasks = [
+{id: 1, content: "przejść na Reacta", done: false },
+{id: 2, content: "zrobić rolety", done: true }
+];
+const hideDoneTasks = false;
 
 function App() {
   return (
-    <div>
-      Planner
-    </div>
+    <main className="container">
+      <header>
+        <h1 className="header">Lista zadań</h1>
+      </header>
+      <section className="section">
+        <h2 className="section__header">Dodaj nowe zadanie</h2>
+        <div className="section__body">
+          <Form />
+        </div>
+      </section>
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Lista zadań</h2>
+          <div className="buttons"></div>
+        </header>
+        <div className="section__body">
+          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+        </div>
+      </section>
+    </main>
   );
 }
 
