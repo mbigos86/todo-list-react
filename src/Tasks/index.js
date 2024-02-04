d7e2542e5c27ae539b6918edcb5f6d7aa69b0acf
Mariusz Bigos <mbrolety@gmail.com>
2024-02-04 21:08:07 +0100

    Add Buttons

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,9 +4,9 @@ const Tasks = (props) => (
     <ul className="tasks">
         {props.tasks.map(task => (
             <li
-            className={`${props.hideDoneTasks && task.done ? "tasks__hidden" : "tasks__item"}`} key={task.id}>   
-             <button className="tasks__button tasks__button--toggleDone"> 
-                {task.done ? "✔" : ""}
+                className={`${props.hideDoneTasks && task.done ? "tasks__hidden" : "tasks__item"}`} key={task.id}>
+                <button className="tasks__button tasks__button--toggleDone">
+                    {task.done ? "✔" : ""}
                 </button>
                 <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}> {task.content} </span>
                 <button className="tasks__button tasks__button--remove">🗑</button>
